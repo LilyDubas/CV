d3aec57748a364eac651f1187dca581d15c77064
LilyDubas <nathdubas@gmail.com>
2020-09-22 19:02:12 +0200

    Ajout du scroll smooth, navbar autoclose on scroll

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
-$('#play').on('click', function (e) {
-  e.preventDefault();
-  $("#player")[0].src += "?autoplay=1";
-  $('#player').show();
-  $('#video-cover').hide();
+// $('#play').on('click', function (e) {
+//   e.preventDefault();
+//   $("#player")[0].src += "?autoplay=1";
+//   $('#player').show();
+//   $('#video-cover').hide();
+// })
+// $('#modal1').on('hidden.bs.modal', function (e) {
+//   $('#modal1 iframe').attr("src", $("#modal1 iframe").attr("src"));
+// });
+// Now starts the good work //
+$('#navbar').on('shown.bs.collapse',function(){
+  $(window).scroll(function(){ $('#navbarSupportedContent').collapse('hide'); })
 })
-$('#modal1').on('hidden.bs.modal', function (e) {
-  $('#modal1 iframe').attr("src", $("#modal1 iframe").attr("src"));
-});
